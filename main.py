79eef03579f364583630692d6432148f68fe3be8
Зиатдинова_Алина <z.alina.r.11.02@gmail.com>
2025-05-19 03:57:14 +0300

    fixed

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score, classification_report
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 from sklearn.metrics import roc_auc_score
 
+df = pd.read_csv("C:/Users/Alina/Downloads/clients.csv")
+
+df["gender"] = LabelEncoder().fit_transform(df["gender"])
+
+features = ["age", "gender", "num_policies", "avg_policy_amount", "support_requests", "days_since_last_policy"]
+X = df[features]
+y = df["churned"]
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+#простая модель
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+y_proba = model.predict_proba(X_test)[:, 1]
+
+print(classification_report(y_test, y_pred))
+print("ROC AUC:", roc_auc_score(y_test, y_proba))
+
+#продвинутая модель
 model = Sequential([
     Dense(64, activation='relu', input_shape=(X_scaled.shape[1],)),
     Dropout(0.3),
@@ -17,3 +46,4 @@ model.fit(X_train, y_train, epochs=20, batch_size=32, validation_split=0.2)
 y_pred_proba = model.predict(X_test).flatten()
 auc = roc_auc_score(y_test, y_pred_proba)
 print("ROC AUC:", auc)
+
